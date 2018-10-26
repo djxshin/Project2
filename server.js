401de85606444c7ac35e071c2cdee84cbc1cfa5f
CB_ATL_105_David_Shin <cb_atl_105_david_shin@CBs-MacBook-Air-9.local>
2018-10-25 23:28:05 -0400

    server.js boiler plate

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 require('dotenv').config();
 
+// boiler plate
+const express = require('express')
+const app = express()
+
+const routes = require('./routes/index')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', routes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
+// end boiler plate
+
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI); 
 
